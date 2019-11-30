5ab42cc43af3db20aa069daf1d66ad1beacf7f23
Theieyrre <crizfmtfm@gmail.com>
2019-11-30 15:13:00 +0300

    Adding modal.css

--- index.php
+++ index.php
@@ -141,6 +141,7 @@ if (isset($_POST['signin'])) {
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 <link rel="stylesheet" href="styles/main.css">
+<link rel="stylesheet" href="styles/modal.css">
 
 <header>
   <nav class="navbar navbar-expand-lg" id=navbar-header>
