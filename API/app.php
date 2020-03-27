9f3dfc8f06fb0bf14620a80ff25a0cc997e8ff91
Theieyrre <crizfmtfm@gmail.com>
2020-03-27 21:43:41 +0300

    Comment PHPMailer

--- API/app.php
+++ API/app.php
@@ -7,12 +7,12 @@
 
 include('../db.php');
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer\src\Exception.php';
-require 'PHPMailer\src\PHPMailer.php';
-require 'PHPMailer\src\SMTP.php';
+//require 'PHPMailer\src\Exception.php';
+//require 'PHPMailer\src\PHPMailer.php';
+//require 'PHPMailer\src\SMTP.php';
 
 $status = "";
 $message = "";
