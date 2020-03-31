e4cc46eea826682d2ff0e9981017e75a99c8c794
dilan dogan <d.dogan@etu.edu.tr>
2020-03-31 14:51:53 +0200

    Creating a different script

--- API/app.php
+++ API/app.php
@@ -7,13 +7,6 @@
 
 include('../db.php');
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer\src\Exception.php';
-require 'PHPMailer\src\PHPMailer.php';
-require 'PHPMailer\src\SMTP.php';
-
 $status = "";
 $message = "";
 $bikes = "";
@@ -492,43 +485,6 @@ if (isset($post_json["username_today"])) {
     );
 
     echo json_encode($json);
-}
-//Recover Email
-if (isset($post_json["recovery_email"]) && isset($post_json["recovery_username"])) {
-    $email = $post_json["recovery_email"];
-    $username = $post_json["recovery_username"];
-    $mail = new PHPMailer(true);
-    try {
-        //Server settings
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'bikepass496@gmail.com';                     // SMTP username
-        $mail->Password   = 'bil496graduationproject';                               // SMTP password
-        $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-        //Recipients
-        $mail->setFrom('recovery@bikepass.com', 'BikePass');
-        $mail->addAddress($email, ' User ' . $username);     // Add a recipient
-        $mail->addReplyTo('no-reply@bikepass.com', 'No reply');
-
-
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Recovery mail from BikePass';
-        $mail->Body    = '<b>Please click the link below to reset your password!</b><br>bikepass.herokuapp.com/changepassword.php';
-
-        $mail->send();
-        $status = 1;
-        $message = "Message has been sent";
-        // echo 'Message has been sent';
-    } catch (Exception $e) {
-        $status = 0;
-        $message = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-    create_response($status, $message, null);
 }
 
 function create_response($status, $message, $bikes)
---
+++ API/recoverymail.php
@@ -0,0 +1,54 @@
+<?php
+include('../db.php');
+
+$status = "";
+$message = "";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer\src\Exception.php';
+require 'PHPMailer\src\PHPMailer.php';
+require 'PHPMailer\src\SMTP.php';
+
+
+//Recover Email
+if (isset($post_json["recovery_email"]) && isset($post_json["recovery_username"])) {
+	$email = $post_json["recovery_email"];
+	$username = $post_json["recovery_username"];
+	$mail = new PHPMailer(true);
+	try {
+		//Server settings
+		$mail->isSMTP();                                            // Send using SMTP
+		$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+		$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+		$mail->Username   = 'bikepass496@gmail.com';                     // SMTP username
+		$mail->Password   = 'bil496graduationproject';                               // SMTP password
+		$mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+		$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+		//Recipients
+		$mail->setFrom('recovery@bikepass.com', 'BikePass');
+		$mail->addAddress($email, ' User ' . $username);     // Add a recipient
+		$mail->addReplyTo('no-reply@bikepass.com', 'No reply');
+
+
+		// Content
+		$mail->isHTML(true);                                  // Set email format to HTML
+		$mail->Subject = 'Recovery mail from BikePass';
+		$mail->Body    = '<b>Please click the link below to reset your password!</b><br>bikepass.herokuapp.com/changepassword.php';
+
+		$mail->send();
+
+		$status = 1;
+		$message = "Message has been sent";
+		// echo 'Message has been sent';
+	} catch (Exception $e) {
+		$status = 0;
+		$message = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+		// echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	}
+
+	$json = array("status" => $status, "message" => $message);
+	echo json_encode($json, JSON_FORCE_OBJECT);
+}
