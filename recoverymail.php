c95c4bbbdd16b708dd158f6a0d3da3d3be29eeeb
dilan dogan <d.dogan@etu.edu.tr>
2020-04-01 22:07:29 +0200

    Trying with ssl port

--- recoverymail.php
+++ recoverymail.php
@@ -30,17 +30,17 @@ if (isset($post_json["recovery_email"]) && isset($post_json["recovery_username"]
 	$mail = new PHPMailer(true);
 	try {
 		//Server settings
-		$mail->isSMTP();                                            // Send using SMTP
-		$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+		//$mail->isSMTP();                                            // Send using SMTP
+		$mail->Host       = "smtp.gmail.com";                    // Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-		$mail->Username   = 'bikepass496@gmail.com';                     // SMTP username
-		$mail->Password   = 'bil496graduationproject';                               // SMTP password
-		$mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-		$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+		$mail->Username   = "bikepass496@gmail.com";                     // SMTP username
+		$mail->Password   = "bil496graduationproject";                               // SMTP password
+		$mail->SMTPSecure = "ssl";         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+		$mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 		//Recipients
-		$mail->setFrom('recovery@bikepass.com', 'BikePass');
-		$mail->addAddress($email, ' User ' . $username);     // Add a recipient
+		$mail->setFrom("recovery@bikepass.com", "BikePass");
+		$mail->addAddress($email, " User " . $username);     // Add a recipient
 		$mail->addReplyTo('no-reply@bikepass.com', 'No reply');
 
 
